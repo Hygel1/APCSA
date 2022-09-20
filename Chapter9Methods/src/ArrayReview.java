c3acb00daf752c9ef5aa942290fd38e805d50260
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2022-09-20 10:30:04 -0400

    9/20/22 Classwork
    Worked on ArrayReview and took notes on Strings

--- Chapter9Methods/src/ArrayReview.java
+++ Chapter9Methods/src/ArrayReview.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 
 //Sample Start File
 public class ArrayReview {
-
-	//#1
+	
 	public static void fillRandom(int[] a) {
 		for(int i=0;i<a.length;i++){
 			a[i]=(int)(Math.random()*100);
@@ -64,7 +63,6 @@ public class ArrayReview {
 		arr[0]=last;
 	}
 	public static void shift(int[] arr, int num){
-		int hold;
 		for(int n=0;n<num;n++){
 			shift(arr);
 		}
@@ -77,6 +75,11 @@ public class ArrayReview {
 	}
 	public static void swap(int[] list, int x, int y){
 		int hold=list[x];
+		list[x]=list[y];
+		list[y]=hold;
+	}
+	public static void swap(String[] list, int x, int y){
+		String hold=list[x];
 		list[x]=list[y];
 		list[y]=hold;
 	}
@@ -113,18 +116,46 @@ public class ArrayReview {
 		}
 	}
 	public static int match(int[] arr, int[] compare){
-		for(int i=0;i<Math.abs(arr.length-compare.length);i++){
-			if(arr.length>=compare.length&&arr[i]==compare[0]){
-				for(int n=0;n<compare.length;n++){
-					//if(compare.)
+		if(arr.length>=compare.length){
+			for(int i=0;i<arr.length-compare.length;i++){
+				if(arr.length>=compare.length&&arr[i]==compare[0]){
+					for(int n=0;n<compare.length;n++){
+						if(compare[n]!=arr[n+i]) break;
+						else if(n==compare.length-1) return i;
+					}
 				}
 			}
 		}
-		return 0;
+		return -1;
+	}
+	/**
+	 * returns the given ara in alphabetical order
+	 */
+	public static String[] alphabetical(String[] arr){
+		int earliestInd;
+		for(int n=0;n<arr.length;n++){
+			earliestInd=n;
+			for(int i=n;i<arr.length;i++){
+				if(arr[i].compareToIgnoreCase(arr[earliestInd])<0){
+					earliestInd=i;
+				} 
+			}
+			swap(arr, n, earliestInd);
+		}
+		return arr;
 	}
 	public static void main(String[] args) {
+		/**
+		 * Still to do:
+		 * Create test method
+		 * Implement comment template and comment before each method (describe each method's function according to assignment)
+		 */
 		int [] numbers = new int[] {3,9,2,7,1,3,9,12,20,2,4};
+		String[] sentences = new String[] {"a", "c", "d", "e", "t", "e", "q"};
 		/*
+		System.out.println(Arrays.toString(alphabetical(new String[] {"swap", "swa", "swb", "sqak"})));
+		
+		System.out.println(match(numbers, new int[] {2, 7, 1, 3}));
 		shift(numbers,2);
 		System.out.println(Arrays.toString(numbers));
 		shift(numbers);
@@ -134,6 +165,7 @@ public class ArrayReview {
 		System.out.println("My list is: " + Arrays.toString(numbers));
 		System.out.println("Smallest is: " + findSmallest(numbers));
 		//void method called on its own line
-		fillRandom(numbers); */
+		fillRandom(numbers); 
+		*/
 	}
 }
