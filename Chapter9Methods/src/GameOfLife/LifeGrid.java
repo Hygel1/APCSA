6fd619fbbf1bcdd0448eb1937abb4accf691edd3
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2022-10-13 19:10:51 -0400

    10/13/2022 Classwork

--- Chapter9Methods/src/GameOfLife/LifeGrid.java
+++ Chapter9Methods/src/GameOfLife/LifeGrid.java
@@ -24,12 +24,14 @@ public class LifeGrid{
         //evolves the gridOfLife by one generation
         public void evolve(){
             int[][] rtn=new int[gridOfLife.length][gridOfLife[0].length]; //temporary array for changing during method
-            for(int i=0;i<gridOfLife.length;i++){
-                for(int n=0;n<gridOfLife[i].length;i++){
-                    int cN=checkNeighbors(i,n);
-                    boolean cS=false; if(gridOfLife[i][n]==ALIVE) cS=true;
-                    if(cS&&(cN<2||cN>3)) rtn[i][n]=DEAD;
-                    else if(!cS&&cN==3) rtn[i][n]=ALIVE;
+            for(int i=0;i<rtn.length;i++){
+                for(int n=0;n<rtn[i].length;n++){
+                    int cN=checkNeighbors(i,n); //cN==current Neighbors
+                    boolean cS=false; if(gridOfLife[i][n]==ALIVE) cS=true; //cS==current state in boolean form
+                    if(cS) rtn[i][n]=ALIVE;
+                    if(cS&&!(cN==2||cN==3)) rtn[i][n]=DEAD; //change from alive to dead
+                    else if(!cS&&cN==3) rtn[i][n]=ALIVE; //change from dead to alive
+                    //if(cS||cN>0) System.out.println("("+i+", "+n+") "+cN); //used to test checkNeighbors
                 }
             }
             gridOfLife=rtn; //releases the memeory of the old gridOfLife and replaces it with rtn
@@ -37,10 +39,37 @@ public class LifeGrid{
         //returns the number of living neighbors around the specified element
         private int checkNeighbors(int i,int j){
             int rtn=0-gridOfLife[i][j];
+            for(int n=i-1;n<i+2;n++){
+                if(n>-1&&n<gridOfLife.length){
+                    for(int q=j-1;q<j+2;q++){
+                        if(q>-1&&q<gridOfLife[0].length) rtn+=gridOfLife[n][q];
+                        else if(q==-1) rtn+=gridOfLife[n][gridOfLife[0].length-1];
+                        else rtn+=gridOfLife[n][0];
+                    }
+                }
+                else if(n==gridOfLife.length-1){
+                    for(int q=j-1;q<j+2;q++){
+                        if(q>-1&&q<gridOfLife[0].length) rtn+=gridOfLife[0][q];
+                        else if(q==-1) rtn+=gridOfLife[0][gridOfLife[0].length];
+                        else rtn+=gridOfLife[0][0];
+                    }
+                }
+                else{
+                    for(int q=j-1;q<j+2;q++){
+                        if(q>-1&&q<gridOfLife[0].length) rtn+=gridOfLife[gridOfLife.length-1][q];
+                        else if(q==-1) rtn+=gridOfLife[gridOfLife.length-1][gridOfLife[0].length-1];
+                        else rtn+=gridOfLife[gridOfLife.length-1][0];
+                    }
+                }
+            }
+            return rtn;
+        }
+        public int checkNeighbors2(int i, int j){
+            int rtn=0;
             for(int n=i-1;n<i+2&&n<gridOfLife.length-1;n++){
                 if(n>-1){
                     for(int q=j-1;q<j+2&&q<gridOfLife[0].length-1;q++){
-                        if(q>-1) rtn+=gridOfLife[n][q];
+                        if(q>-1&&!(n==i||q==j)) rtn+=gridOfLife[n][q];
                     }
                 }
             }
