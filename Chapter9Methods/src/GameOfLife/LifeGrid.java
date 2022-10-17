8c694798fb81dfd104570766b977e3605c54f68a
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2022-10-17 09:36:23 -0400

    10/17/2022 Classwork

--- Chapter9Methods/src/GameOfLife/LifeGrid.java
+++ Chapter9Methods/src/GameOfLife/LifeGrid.java
@@ -8,10 +8,11 @@ public class LifeGrid{
      * full comments in GameOfLife.java
      */
      LifeGrid(int i, int j){
+        //super(new GameOfLife());
         gridOfLife=new int[i][j];
     }
         public static void main(String[] args){
-
+            
         }
         // returns the value of a specified element
         public int getCell(int i, int j){
@@ -25,21 +26,52 @@ public class LifeGrid{
         public void evolve(){
             int[][] rtn=new int[gridOfLife.length][gridOfLife[0].length]; //temporary array for changing during method
             for(int i=0;i<rtn.length;i++){
+                boolean brk=false;
                 for(int n=0;n<rtn[i].length;n++){
                     int cN=checkNeighbors(i,n); //cN==current Neighbors
-                    boolean cS=false; if(gridOfLife[i][n]==ALIVE) cS=true; //cS==current state in boolean form
+                    boolean cS=gridOfLife[i][n]==ALIVE; //cS==current state in boolean form
+                    if(cN==8){ //If any block is fully surrounded, kill everything on screen
+                       killAll();
+                       brk=true;
+                       break;
+                    } 
                     if(cS) rtn[i][n]=ALIVE; //Set value in rtn to its setting in gridOfLife
                     if(cS&&!(cN==2||cN==3)) rtn[i][n]=DEAD; //change from alive to dead
                     else if(!cS&&cN==3) rtn[i][n]=ALIVE; //change from dead to alive
                     //if(cS||cN>0) System.out.println("("+i+", "+n+") "+cN); //used to test checkNeighbors
                 }
+                if(brk) break;
             }
             gridOfLife=rtn; //releases the memeory of the old gridOfLife and replaces it with rtn
+        }
+        public void killAll(){
+            for(int i=0;i<gridOfLife.length;i++){
+                for(int n=0;n<gridOfLife[i].length;i++){
+                    gridOfLife[i][n]=DEAD;
+                }
+            }
+            //repaint();
+            /*for(int i=0;i<gridOfLife.length;i++){
+                for(int n=0;n<gridOfLife[i].length;n++){
+                    gridOfLife[i][n]=DEAD;
+                }
+                //repaint();
+            } */
         }
         //returns the number of living neighbors around the specified element
         private int checkNeighbors(int i,int j){
             int rtn=0-gridOfLife[i][j]; //if the origin value will add, subtract to counteract
-            for(int n=i-1;n<i+2;n++){ 
+            for(int n=i-1;n<i+2;n++){
+                int x;
+                if(n>-1&&n<gridOfLife.length)x=n; //x-central
+                else if(n==gridOfLife.length)x=0; //right-bound, replace all x-values with 0
+                else x=gridOfLife.length-1; //left-bound by eliminaion, replace all x-values with rightmost value
+                for(int q=j-1;q<j+2;q++){
+                    if(q>-1&&q<gridOfLife[0].length) rtn+=gridOfLife[x][q]; //y-central, perform normally
+                    else if(q==-1) rtn+=gridOfLife[x][gridOfLife[0].length-1];  //top-bound, consider the bottom value rather than the value above
+                    else rtn+=gridOfLife[x][0]; //bottom-bound, consider the top value rather than the value below
+                }
+                /* less efficient version of rounding the array
                 if(n>-1&&n<gridOfLife.length){ //x-central
                     for(int q=j-1;q<j+2;q++){
                         if(q>-1&&q<gridOfLife[0].length) rtn+=gridOfLife[n][q]; //y-central, perform normally
@@ -62,6 +94,7 @@ public class LifeGrid{
                         else rtn+=gridOfLife[gridOfLife.length-1][0];
                     }
                 }
+                */
             }
             return rtn;
         }
---
+++ Chapter9Methods/src/abstractionPractice/theBaby.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class theBaby extends className{
+    public static void main(String[] args){
+        theClass();
+        System.out.println(minor.y); //references inner class
+        System.out.println(newClass.myNumber()); //directly references parent class' inner class
+        //note: when the parent's inner class and/or the method were private, this was not allowed
+    }
+    static class minor{ //nested class defines variable to be used in outer class
+    //staticising this class makes it able to be used without referencing the parent class
+        static int y=0;
+    }
+}
