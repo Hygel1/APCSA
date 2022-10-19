51749ba66bb2f9a5b1ea7f11cceeabe24812bebd
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2022-10-18 20:13:29 -0400

    10/18/2022

--- Chapter9Arrays/MatrixWork/MatrixWorkProblems.java
+++ Chapter9Arrays/MatrixWork/MatrixWorkProblems.java
@@ -1,14 +1,9 @@
+import java.util.Arrays;
 public class MatrixWorkProblems {
-    public class Color{
-        public char state;
-        public Color(boolean c){
-            if(c) state='x';
-            else state=' ';
-        }
-    }
-
 public static void main(String args[]){
-
+    Color[][] box=new Color[3][3];
+    fillCheckerboard(box);
+    printCheckerboard(box);
 }
 public static void q13(){
 
@@ -22,11 +17,36 @@ public static double positiveMax(double[][] m){
     }
     return max;
 }
+public static class Color{
+    char rtn;
+    public Color(boolean c){
+        if(c) rtn='x';
+        else rtn='.';
+    }
+    public String toString(){
+        return rtn+"";
+    }
+}
 public static void fillCheckerboard(Color[][] board){
     for(int i=0;i<board.length;i++){
         for(int n=0;n<board[i].length;n++){
-            
+            if(i%2==0){
+                if(n%2==0)board[i][n]=new Color(true);
+                else board[i][n]=new Color(false);
+            }
+            else{
+                if(n%2==0)board[i][n]=new Color(false);
+                else board[i][n]=new Color(true);
+            }
         }
+    }
+}
+public static void printCheckerboard(Color[][] board){
+    for(int i=0;i<board.length;i++){
+        for(int n=0;n<board[i].length;n++){
+            System.out.print(board[i][n]+" ");
+        }
+        System.out.println();
     }
 }
 }
