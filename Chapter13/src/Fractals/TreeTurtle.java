f1fe732a78e12b24cff3dc92897e9b460478d93a
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2023-01-24 20:18:20 -0500

    1/24/22 Classwork

---
+++ Chapter13/src/Fractals/TreeTurtle.java
@@ -0,0 +1,118 @@
+package Fractals;
+import java.awt.Color;
+
+import edu.gatech.mediaprogramming.*; //GT turtle library
+/**
+ * turtle commands
+ * forward(int distance) - moves forward
+ * backward(int distance) - moves backward
+ * turn(int degrees)
+ */
+
+public class TreeTurtle extends Turtle{ //Turtle is part of turtle.jar and imported from edu.gatech.mediaprogramming.*
+    public static void main(String[] args){
+        System.out.println("yo");
+        TreeTurtle t=new TreeTurtle();
+        t.penUp();
+        t.moveTo(100, 350);
+        t.setColor(Color.BLACK);
+        t.penDown();
+        t.turn(90);
+        //t.drawTree(9, 200);
+        //t.drawKochCurve(9, 500);
+        //t.drawSierpinski(8, 350);
+    }
+    /**
+     * turtle will always return to its initial state at the base of the tree just drawn
+     * @param depth - depth of recursion, decremented as the program runs
+     * @param distance - length of the trunk at the given stage
+     */
+    public void drawTree(int depth, double distance){
+        if(depth==1){ 
+            forward(distance);
+            backward(distance); //return to base
+        }
+        else{
+            forward(distance);
+            turn(45);
+            drawTree(depth-1,distance/2);
+            turn(-90); //turn back and 45 degrees past original angle
+            drawTree(depth-1,distance/2);
+            turn(45); //go back to original angle
+            backward(distance);
+        }
+    }
+    /**
+     * draws a koch curve using recursion
+     * @param depth depth to recurse, decrements by one each call
+     * @param length split into 3 parts
+     */
+    public void drawKochCurve(int depth, double length){
+        if(depth==0){
+            forward(length);
+        }
+        else{
+            drawKochCurve(depth-1, length/3);
+            turn(-60);
+            drawKochCurve(depth-1, length/3);
+            turn(120);
+            drawKochCurve(depth-1, length/3);
+            turn(-60);
+            drawKochCurve(depth-1, length/3);
+        }
+    }
+    /**
+     * draws sierpinski's triangle using recursion
+     * @param depth depth to recurse, decrements by one
+     * @param side length of triangle's side, split in two
+     */
+    public void drawSierpinski(int depth, double side){
+        if(depth==0){
+            forward(side);
+            turn(-120);
+            forward(side);
+            turn(-120);
+            forward(side);
+            turn(-120);
+        }
+        else{
+            drawSierpinski(depth-1, side/2);
+            forward(side/2);
+            drawSierpinski(depth-1, side/2);
+            turn(-120);
+            forward(side/2);
+            turn(120);
+            drawSierpinski(depth-1, side/2);
+            turn(120);
+            forward(side/2);
+            turn(-120);
+        }
+    }
+    public static void drawPascal(int times, int number){
+        
+    }
+    public static void mandelbrot(){
+
+
+    }
+    public class ComplexNumber{
+        double real,com;
+        public ComplexNumber(double real, double com){
+            this.real=real;
+            this.com=com;
+        }
+        public double getReal(){
+            return real;
+        }
+        public double getComplex(){
+            return com;
+        }
+        public ComplexNumber add(ComplexNumber n1, ComplexNumber n2){
+            return new ComplexNumber(n1.getReal()+n2.getReal(),n1.getComplex()+n2.getComplex());
+        }
+        public ComplexNumber multiply(ComplexNumber n1,ComplexNumber n2){
+            if(n1.getComplex()==0&&n2.getComplex()==0) return new ComplexNumber(n1.getReal()*n2.getReal(), 0);
+            else if(n1.getComplex()!=0&&n2.getComplex()==0)
+        }
+    }
+}
