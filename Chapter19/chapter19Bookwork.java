8ee6e43643d39de4685f546210c136334ec16a81
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2023-03-20 19:34:22 -0400

    Update chapter19Bookwork.java

--- Chapter19/chapter19Bookwork.java
+++ Chapter19/chapter19Bookwork.java
@@ -6,12 +6,20 @@ public class chapter19Bookwork {
      * 
      * Base Case: 
      *  mS(1)=1                         (works)
-     * Test: mS(n)=mS(n-1)+2*n-1        (works)
+     * Test: mS(n)=mS(n-1)+2*n-1        
      *  mS(2)=mS(1)+2*2-1=1+4-1=4       (works)
-     *  mS(3)=mS(2)+2*3-1=4+2*3-1=9     (works)
-     *  mS(4)=mS(3)+2*4-1=9+8-1=16      (works)
+     * 
+     * (n-1)^2+2*n-1=n^2-n-n+1+2*n-1=n^2-2n+2n+1-1=n^2
+     * 
+     * it would be easier and less resource intensive to use a for loop
      */
     /**
-     * Q7: 
+     * Q7: N=number of people in the set        n=possible depth
+     * Base Case:
+     *  dS(1)=1;
+     * Test: (3*N^n-N^(n-1)-2*N)/(N-1)
+     *  dS(2)=(3*N^2-N^(1)-2*N)/(N-1)=
+     *dS(n)=()
+     *  
      */
 }
