0ac72e243fa0d1414836fc774f290a4dd158f71d
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2023-03-20 18:48:01 -0400

    3/20/23 CW

---
+++ Chapter19/chapter19Bookwork.java
@@ -0,0 +1,17 @@
+package Chapter19;
+
+public class chapter19Bookwork {
+    /**
+     * Q6: Hypothesis mysterySum(n)=n^2
+     * 
+     * Base Case: 
+     *  mS(1)=1                         (works)
+     * Test: mS(n)=mS(n-1)+2*n-1        (works)
+     *  mS(2)=mS(1)+2*2-1=1+4-1=4       (works)
+     *  mS(3)=mS(2)+2*3-1=4+2*3-1=9     (works)
+     *  mS(4)=mS(3)+2*4-1=9+8-1=16      (works)
+     */
+    /**
+     * Q7: 
+     */
+}
