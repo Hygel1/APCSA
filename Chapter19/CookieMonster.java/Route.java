2b3ac14c9b3d52278f45f05b829943517f54fb1a
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2023-03-30 08:59:54 -0400

    Update Route.java

--- Chapter19/CookieMonster.java/Route.java
+++ Chapter19/CookieMonster.java/Route.java
@@ -1,4 +1,11 @@
 import java.util.Stack;
-public class Route extends Stack<Point>{
-
+public class Route extends Stack<int[]>{
+    private int cost=0;
+    public Route(Route r, int[] i){
+        cost=r.cost;
+        new Stack(this);
+    }
+    public int getCost(){
+        return cost;
+    }
 }
